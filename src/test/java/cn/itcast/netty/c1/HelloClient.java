41aa27bc3ce6de189e3e72e0358a5d9056302ee0
陈友增 <409121961@qq.com>
2022-09-09 22:26:40 +0800

    feat(Netty入门)：2.Hello World

---
+++ src/test/java/cn/itcast/netty/c1/HelloClient.java
@@ -0,0 +1,29 @@
+package cn.itcast.netty.c1;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.net.InetSocketAddress;
+import java.time.LocalDateTime;
+
+public class HelloClient {
+    public static void main(String[] args) throws InterruptedException {
+        new Bootstrap()
+            .group(new NioEventLoopGroup())
+            .channel(NioSocketChannel.class)
+            .handler(new ChannelInitializer<Channel>() {
+                @Override
+                protected void initChannel(Channel ch) throws Exception {
+                    ch.pipeline().addLast(new StringEncoder());
+                }
+            })
+            .connect(new InetSocketAddress("localhost", 8080))
+            .sync()
+            .channel()
+            .writeAndFlush(LocalDateTime.now() + ": hello world");
+    }
+}
--- src/test/java/cn/itcast/netty/c1/HelloServer.java
+++ src/test/java/cn/itcast/netty/c1/HelloServer.java
@@ -1,8 +1,32 @@
 package cn.itcast.netty.c1;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+
 public class HelloServer {
 
     public static void main(String[] args) {
-        
+        new ServerBootstrap()
+            .group(new NioEventLoopGroup())
+            .channel(NioServerSocketChannel.class)
+            .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                @Override
+                protected void initChannel(NioSocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new StringDecoder());
+                    ch.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
+                        @Override
+                        protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+                            System.out.println(msg);
+                        }
+                    });
+                }
+            })
+            .bind(8080);
     }
 }
